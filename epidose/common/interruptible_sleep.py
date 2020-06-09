b624bc6684876298e4b510f5f929c728b6bc4a79
Diomidis Spinellis <dds@aueb.gr>
2020-06-09 17:10:39 +0300

    Add interruptible sleep utility class

---
+++ epidose/common/interruptible_sleep.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+""" A sleep function that can be detectably interrupted """
+
+__copyright__ = """
+    Copyright 2020 Diomidis Spinellis
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+__license__ = "Apache 2.0"
+
+from threading import Event
+import signal
+
+
+class InterruptableSleep:
+    """ Provide a sleep-like function that can be interrupted and
+    by a detectable signal.  See https://stackoverflow.com/a/46346184/20520
+    """
+
+    def __init__(self, sig):
+        """ Create an object for the specified signal """
+        signal.signal(sig, self.sigterm_handler)
+        self.event = Event()
+        self.signaled = False
+
+    def sleep(self, t):
+        """ Sleep for the specified time interval, or less if the signal
+        is received.  The "signaled" member can be examined to see if a
+        signal was received, while sleeping. """
+        self.event.clear()
+        self.signaled = False
+        self.event.wait(t)
+
+    def sigterm_handler(self, signum, frame):
+        self.signaled = True
+        self.event.set()
+
+
+def main():
+    sleeper = InterruptableSleep(signal.SIGTERM)
+    print("Expect to see a 'signaled' message when you send a TERM signal")
+    while True:
+        print("Sleeping")
+        sleeper.sleep(5)
+        if sleeper.signaled:
+            print("Signaled")
+        print("Looping")
+
+
+if __name__ == "__main__":
+    main()
